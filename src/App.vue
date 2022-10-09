2958cc10d0148490f5691cbc4aadf47203ede670
HBorch <31587159+HBorch@users.noreply.github.com>
2022-10-09 22:14:05 +0200

    lagt til bilde

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
+    <img alt="bildeBodilogHenrik" class="responsivephoto" src="@/assets/bodiloghenrik3.jpg">
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
